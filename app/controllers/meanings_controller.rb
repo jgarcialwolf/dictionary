dd5d3d4e0f398d53d94135307a4fb90757e9c31d
Javier Garcia <jgarcia@lwolf.com>
2022-06-15 14:51:19 -0500

    add index page for meanings resource

--- app/controllers/meanings_controller.rb
+++ app/controllers/meanings_controller.rb
@@ -1,9 +1,11 @@
 class MeaningsController < ApplicationController
 
+  def index
+    @meanings = Meaning.all
+  end
+  
   def show
-
     @meaning = Meaning.find(params[:id])
-
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   root "pages#index"
-  # get "/pages", to: "pages#index"
   resources :meanings
 
 end
