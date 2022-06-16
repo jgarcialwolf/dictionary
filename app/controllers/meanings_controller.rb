1b175098f9daf987b6783bdfc288158678e3ca05
Javier Garcia <jgarcia@lwolf.com>
2022-06-15 20:08:32 -0500

    add page with form for new meaning

--- app/controllers/meanings_controller.rb
+++ app/controllers/meanings_controller.rb
@@ -7,5 +7,16 @@ class MeaningsController < ApplicationController
   def show
     @meaning = Meaning.find(params[:id])
   end
+
+  def new
+    
+  end
+  
+  def create
+    @meaning = Meaning.new(params.require(:meaning).permit(:english_word, :translation))
+    @meaning.save
+    redirect_to @meaning
+  end
+  
   
 end
