ffa94c2822218ad8b57d4580e80a41002d72063c
Javier Garcia <jgarcia@lwolf.com>
2022-06-15 22:08:46 -0500

    add edit feature to meanings resource

--- app/controllers/meanings_controller.rb
+++ app/controllers/meanings_controller.rb
@@ -11,6 +11,10 @@ class MeaningsController < ApplicationController
   def new
     @meaning = Meaning.new
   end
+
+  def edit
+    @meaning = Meaning.find(params[:id])
+  end
   
   def create
     @meaning = Meaning.new(params.require(:meaning).permit(:english_word, :translation))
@@ -20,8 +24,16 @@ class MeaningsController < ApplicationController
     else
       render :new, status: 422
     end
-    
   end
-  
+
+  def update
+    @meaning = Meaning.find(params[:id])
+    if @meaning.update(params.require(:meaning).permit(:english_word, :translation))
+      flash[:notice] = "Meaning was updated successfully"
+      redirect_to @meaning
+    else
+      render :edit, status: 422
+    end
+  end
   
 end
