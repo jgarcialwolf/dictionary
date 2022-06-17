e9eaa2c05159ddda8558615732b9192b6340e08f
jgarcialwolf <jgarcia@lwolf.com>
2022-06-16 21:23:39 -0500

    Meanings resources (#1)
    * add migration file for meanings table
    
    * add model with validation
    
    * modify routes and add show view and controller
    
    * add index page for meanings resource
    
    * fix indentation
    
    * add page with form for new meaning
    
    * add validation and flash messages
    
    * add edit feature to meanings resource
    
    * add destroy feature to meanings resource
    
    * add layout links

---
+++ app/models/meaning.rb
@@ -0,0 +1,4 @@
+class Meaning < ApplicationRecord
+  validates :english_word, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 1, maximum: 20}
+  validates :translation, presence: true, length: {minimum: 1, maximum: 20}
+end
