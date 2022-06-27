52da0a53101d21592ba1b07c8bd5c8dc63ed79f3
Javier Garcia <jgarcia@lwolf.com>
2022-06-27 01:03:33 -0500

    downcase email before saving it to db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :meanings
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 3..30 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
