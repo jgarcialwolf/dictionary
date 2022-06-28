7ad2b42788c05baed6fd428a1241f797abde8b65
jgarcialwolf <jgarcia@lwolf.com>
2022-06-27 22:41:46 -0500

    Create users (#3)
    * add users table and model
    
    * add validation to user
    
    * add one-to-many association between users and meanings plus primary key migration
    
    * downcase email before saving it to db
    
    * add secure password to users
    
    * add signup form

--- app/models/meaning.rb
+++ app/models/meaning.rb
@@ -1,4 +1,5 @@
 class Meaning < ApplicationRecord
+  belongs_to :user
   validates :english_word, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 1, maximum: 20}
   validates :translation, presence: true, length: {minimum: 1, maximum: 20}
 end
