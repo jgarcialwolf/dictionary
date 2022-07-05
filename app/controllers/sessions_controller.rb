c3c6ae603c3ffa54524d606c3c4668954b60a224
Javier Garcia <jgarcia@lwolf.com>
2022-07-05 00:44:58 -0500

    add routes, login template plus create and destroy action for sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome #{user.username}"
+      redirect_to user
+    else
+      flash.now[:alert] = "Something went wrong!!"
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
+  end
+  
+end
